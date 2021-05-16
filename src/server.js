7cf26aa60ba1c7e57dd908b9aa903ee5961c062e
zanuarts <nuartea27@gmail.com>
2021-05-16 17:42:22 +0700

    add create

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes')
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost'
+    })
+
+    server.route(routes)
+
+    await server.start()
+    console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
