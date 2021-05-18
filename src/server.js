7b12235cfeaa5a739c53326b256f5ec04c878b87
zanuarts <nuartea27@gmail.com>
2021-05-18 17:44:52 +0700

    add save notes

--- src/server.js
+++ src/server.js
@@ -13,4 +13,14 @@ const init = async () => {
     console.log(`server berjalan pada ${server.info.uri}`);
 };
 
+const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+        cors: {
+            origin: ['*'],
+        }
+    }
+})
+
 init();
